653e10d2eab55395876eb9081ecf1c4bcea2f2e4
SindyOquendo <oquendo1913@gmail.com>
2021-10-16 18:11:19 -0500

    adelanto desafío componentes II

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,31 +2,33 @@ import  {useState} from "react";
 import { Button  } from "react-bootstrap";
 import "./itemCount.css";
 
-
-const Product = ({ price, name,  descripcion,stock, CustomButton }) => {
+const Product = ({ price, name, descripcion, stock}) => {
 
   const [count, setCount] = useState (0);  
   const Add = () =>setCount (count +1);
   const subtract= () =>setCount (count -1);
+  const agregar = ()=> setCount (0);
   
-    return (
+  
+  return (
       <>      
         <div>
           <h2>Nombre: {name}</h2>
           <h3>Precio: {price}</h3>   
           <h5>Descripción:{descripcion}</h5>
-          <h5>stock:{stock}</h5>
+          <h5>stock:{stock}</h5>          
           <div className="cart-button">
           <p>Cantidad</p>
-          <Button variant="light btnLight" onClick= {Add}> +  {CustomButton}</Button>
+          <Button variant="light btnLight" onClick= {Add}>+</Button>
           <div className= "count-cart">{count}</div>
-          <Button variant="light btnLight" onClick= {subtract}> - {CustomButton}</Button>
-          <Button variant="light btnLight"> Agregar al Carro</Button>
+          <Button variant="light btnLight" onClick= {subtract}>-</Button>
+          <Button variant="light btnLight" onClick= {agregar}> Vaciar item </Button>
+          <Button  variant="light btnLight" onClick={() => setCount(count + 1)}>Agregar al Carro</Button>
           </div>
         </div>
         <hr />
       </>
     );
   };
-  
+   
   export default Product;
